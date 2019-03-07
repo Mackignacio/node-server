c92c1d0cec62250e07791cc44a6ae9df037c99dc
Mack_Ignacio <mack_ignacio@yahoo.com>
2019-03-08 01:39:22 +0800

    Update models/index.ts

---
+++ src/models/index.ts
@@ -0,0 +1,9 @@
+import { users } from "./lib/users.model";
+import { products } from "./lib/products.model";
+
+const models: { [k: string]: any } = {
+  users,
+  products,
+};
+
+export default models;
