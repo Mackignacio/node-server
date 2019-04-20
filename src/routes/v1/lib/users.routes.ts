917b873dd15f711ecad4649606e9802eef89a8ef
Mack <mack_ignacio@yahoo.com>
2019-04-20 15:19:30 +0800

    Moved to v1/lib

--- src/routes/v1/lib/users.routes.ts
+++ src/routes/v1/lib/users.routes.ts
@@ -1,17 +1,10 @@
-import { Router, Request, Response } from "express";
-import config from "../../helper/config";
-import jwt from "../../middleware/jwt";
-import models from "../../models";
+import { Request, Response } from "express";
+import config from "../../../helper/config";
+import jwt from "../../../middleware/jwt";
+import models from "../../../models";
 
-class Users {
-  router = Router();
+export class Users {
   model = models["users"];
-
-  constructor() {
-    this.router.get("/", (req: Request, res: Response) => this.getAllUsers(req, res));
-    this.router.post("/", jwt.verifyToken, (req: Request, res: Response) => this.addUser(req, res));
-    this.router.post("/login", (req: Request, res: Response) => this.getAllUsers(req, res));
-  }
 
   async getAllUsers(req: Request, res: Response) {
     // const result = await this.model.find({}).populate("product_id");
@@ -48,5 +41,3 @@ class Users {
     res.json({ message: "Post created", token: (<any>req)["token"], decoded: (<any>req)["decoded"] });
   }
 }
-
-export const users = new Users().router;
