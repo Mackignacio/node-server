9430f93e266579176f2f008b7a763b2f009b4068
Mack <mack_ignacio@yahoo.com>
2019-04-20 15:52:23 +0800

    Add Model class

--- src/models/index.ts
+++ src/models/index.ts
@@ -5,3 +5,15 @@ const models: { [k: string]: any } = {
 };
 
 export default models;
+
+export class Models {
+  private models: { [k: string]: any } = { users };
+
+  find(name: string) {
+    return this.models[name];
+  }
+
+  create(name: string, value: {}) {
+    return new models[name](value);
+  }
+}
