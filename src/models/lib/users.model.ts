d3fc4b21c2a5d7d990753b410792b3f4b3636ea9
Mac Ignacio <racketstudio@Macs-MacBook-Pro.local>
2019-04-20 13:29:39 +0800

    Update users model

--- src/models/lib/users.model.ts
+++ src/models/lib/users.model.ts
@@ -4,25 +4,25 @@ import paginate from "mongoose-paginate";
 const SchemaOptions = {
   timestamps: {
     createdAt: "DateCreated",
-    updatedAt: "DateUpdated",
-  },
+    updatedAt: "DateUpdated"
+  }
 };
 
 const UserSchema = new Schema(
   {
     name: {
       type: String,
-      required: [true, "Name is required."],
+      required: [true, "Name is required."]
     },
     account_type: {
       type: String,
-      required: [true, "Price is required."],
+      required: [true, "Price is required."]
     },
     product_id: {
       type: Schema.Types.ObjectId,
       ref: "Product",
-      required: [true, "Product ID is required."],
-    },
+      required: [true, "Product ID is required."]
+    }
   },
   SchemaOptions
 );
