f1a25b9aecf5c85fc1129f6f0cb01e48db8a8852
Mac Ignacio <racketstudio@Macs-MacBook-Pro.local>
2019-04-20 13:38:05 +0800

    Add v1 routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,12 @@
 import { Express } from "express";
 import { users } from "./lib/users.routes";
-import { products } from "./lib/products.routes";
 
-export const Routes = (app: Express) => {
-  // ROUTES
-  app.use("/users", users);
-  app.use("/products", products);
-};
+export class Router {
+  constructor(private app: Express) {
+    this.app = app;
+  }
+
+  version1() {
+    this.app.use("/v1/users", users);
+  }
+}
